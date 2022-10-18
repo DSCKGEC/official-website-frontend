d81213fe62c0a35d0bc084616309f9db267c12e5
sudip101-arc <maitysudip6524@gmail.com>
2022-10-18 21:48:10 +0530

    started hero

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,102 +1,98 @@
 import React, { useState } from "react";
-import { Menu } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import DSCLogo from "../../assets/dsc-logo.png";
 import "./Sidebar.scss";
-import Routes from "./menuHelper";
-
-const { SubMenu } = Menu;
-
-const rootSubmenuKeys = ["sub1", "sub2", "sub3"];
 
 const Sidebar: React.FC<IContentMovedProps> = ({
   contentMoved,
   setContentMoved,
 }) => {
+  const location = useLocation();
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
-  const [openKeys, setOpenKeys] = useState<string[]>([]);
-
-  const onOpenChange = (keys: any) => {
-    const latestOpenKey = keys.find((key: any) => openKeys.indexOf(key) === -1);
-    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-    }
-  };
+  console.log(location.pathname);
   return (
     <div className={!sidebarOpen ? "sidebar opened" : "sidebar closed"}>
-      <div className="top">
-        <div className="top-bar">
-          <h2 className="logo none-text">GDSC-KGEC</h2>
-          <div
-            className={
-              !sidebarOpen ? "menu-toggle menu-toggle-light" : "menu-toggle"
-            }
-            onClick={() => {
-              setSidebarOpen(!sidebarOpen);
-              setContentMoved(sidebarOpen);
-            }}
-          >
-            <span className="menu-toggle-bar one"></span>
-            <span className="menu-toggle-bar two"></span>
-            <span className="menu-toggle-bar three"></span>
+      <div className="top-bar">
+        {/* <h2 className="logo none-text">GDSC-KGEC</h2> */}
+        <div
+          className={
+            !sidebarOpen ? "menu-toggle menu-toggle-light" : "menu-toggle"
+          }
+          onClick={() => {
+            setSidebarOpen(!sidebarOpen);
+          }}
+        >
+          <span className="menu-toggle-bar one"></span>
+          <span className="menu-toggle-bar two"></span>
+          <span className="menu-toggle-bar three"></span>
+        </div>
+      </div>
+      <nav className="nav-container">
+        <div className="title-text">
+          <div className="logo-img-div">
+            <img src={DSCLogo} alt="" className="logo" />
+          </div>
+          <div className="logo-text-content">
+            <span>Google Developer Students Club</span>
+            <span>KGEC</span>
           </div>
         </div>
-        <nav className="nav-container">
-          <Menu
-            mode="inline"
-            openKeys={openKeys}
-            onOpenChange={onOpenChange}
-            style={{ width: 256 }}
-          >
-            {Routes.map((route) =>
-              route.children ? (
-                <SubMenu key={route.key} icon={route.icon} title={route.title}>
-                  {route.children.map((child) => (
-                    <Menu.Item key={child.key}>
-                      {child.link ? (
-                        <Link
-                          to={child.link}
-                          className="menu-link"
-                          onClick={() => {
-                            if (sidebarOpen) setSidebarOpen(false);
-                            if (sidebarOpen) setContentMoved(true);
-                          }}
-                        >
-                          {child.title}
-                        </Link>
-                      ) : (
-                        child.title
-                      )}
-                    </Menu.Item>
-                  ))}
-                </SubMenu>
-              ) : (
-                <Menu.Item key={route.key} icon={route.icon}>
-                  {route.link ? (
-                    <Link
-                      to={route.link}
-                      className="menu-link"
-                      onClick={() => {
-                        if (sidebarOpen) setSidebarOpen(false);
-                        if (sidebarOpen) setContentMoved(true);
-                      }}
-                    >
-                      {route.title}
-                    </Link>
-                  ) : (
-                    route.title
-                  )}
-                </Menu.Item>
-              )
-            )}
-          </Menu>
-        </nav>
-      </div>
-      <div className="bottom btn-add-room">
-        <i className="fa-solid fa-circle-plus"></i>
-        <span className="btn-title">Add Room</span>
-      </div>
+        <ul className="nav-links">
+          <li>
+            <Link
+              to="/"
+              style={{ color: location.pathname === "/" ? "$blue" : "$dark" }}
+              onClick={() => setSidebarOpen(true)}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              style={{
+                color: location.pathname === "/about" ? "$blue" : "$dark",
+              }}
+              onClick={() => setSidebarOpen(true)}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/blogs"
+              style={{
+                color: location.pathname === "/blogs" ? "$blue" : "$dark",
+              }}
+              onClick={() => setSidebarOpen(true)}
+            >
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/projects"
+              style={{
+                color: location.pathname === "/projects" ? "$blue" : "$dark",
+              }}
+              onClick={() => setSidebarOpen(true)}
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/events"
+              style={{
+                color: location.pathname === "/events" ? "$blue" : "$dark",
+              }}
+              onClick={() => setSidebarOpen(true)}
+            >
+              Events
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,36 +1,23 @@
 import React from "react";
+import DSCLogo from "../../assets/dsc-logo.png";
 import "./Home.scss";
 
 const Home: React.FC = () => {
-  const LineChart = {
-    options: {
-      chart: {
-        id: "basic-bar",
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-      },
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91],
-      },
-    ],
-  };
-  const PieChart = {
-    options: {
-      dataLabels: {
-        enabled: true,
-        formatter: (val: any) => {
-          return val.toFixed(2) + "%";
-        },
-      },
-    },
-    series: [44, 55, 41, 17, 15],
-    labels: ["A", "B", "C", "D", "E"],
-  };
-  return <section className="home-container">Home</section>;
+  return (
+    <div className="home-container container">
+      <div className="home-wrapper wrapper">
+        <div className="hero-text-box">
+          <div className="logo-img-div">
+            <img src={DSCLogo} alt="" className="logo" />
+          </div>
+          <div className="text-content">
+            <h2>Google Developer Students Club</h2>
+            <h4>Kalyani Government Engineering College</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
